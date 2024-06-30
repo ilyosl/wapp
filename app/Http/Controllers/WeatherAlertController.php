f66e23ae71f387068895a5cb46691040d7a510aa
Ilyosl <ilyosl@mail.ru>
2024-06-30 14:23:04 +0500

    adding files

--- app/Http/Controllers/WeatherAlertController.php
+++ app/Http/Controllers/WeatherAlertController.php
@@ -2,13 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\WeatherServices;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class WeatherAlertController extends Controller
 {
+    protected $weatherService;
+
+    public function __construct(WeatherServices $weatherService)
+    {
+        $this->weatherService = $weatherService;
+    }
+
+
     public function index(){
+
         $user = Auth::user();
         return view('dashboard', compact('user'));
+
+    }
+
+    public function show(Request $request)
+    {
+        $data = $this->weatherService->getWeather($request->latitude, $request->longitude);
+
+        return response()->json($data);
     }
 }
--- app/Services/WeatherServices.php
+++ app/Services/WeatherServices.php
@@ -4,9 +4,30 @@
 namespace App\Services;
 
 
+use Illuminate\Support\Facades\Http;
+
 class WeatherServices
 {
-    public function weatherInfo(){
-        
+    protected $apiKey;
+
+    public function __construct()
+    {
+        $this->apiKey = env('WEATHER_API_KEY');
+    }
+
+    public function getWeather($latitude, $longitude)
+    {
+        $response = Http::get("https://api.openweathermap.org/data/2.5/onecall", [
+            'lat' => $latitude,
+            'lon' => $longitude,
+            'appid' => $this->apiKey,
+            'units' => 'metric'
+        ]);
+
+        if ($response->successful()) {
+            return $response->json();
+        }
+
+        return null;
     }
 }
